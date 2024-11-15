6c99836dbdb4429dd09f134a43121d7694e0eb85
RC Maples <rc@rcmaples.io>
2024-11-14 20:28:27 -0500

    final styling

--- components/NyanCat/index.tsx
+++ components/NyanCat/index.tsx
@@ -1,53 +1,50 @@
-import './styles.css';
+import "./styles.css";
 
-import { FC, useState } from 'react';
+import React, { FC, useState } from "react";
 
 export const NyanCat: FC = () => {
   const [currentIndex, setCurrentIndex] = useState(3);
 
-  const positions = ['minimal', 'left', 'middle', 'right'];
+  const positions = ["minimal", "left", "middle", "right"];
 
-  const [facingDirection, setFacingDirection] = useState('right');
+  const [facingDirection, setFacingDirection] = useState("right");
 
   const onClick = () => {
     if ((currentIndex + 1) % positions.length == 0) {
-      setFacingDirection('left');
+      setFacingDirection("left");
     } else {
-      setFacingDirection('right');
+      setFacingDirection("right");
     }
 
     setCurrentIndex((prevIndex) => (prevIndex + 1) % positions.length);
   };
 
   const onTransitionEnd = (e: React.TransitionEvent<HTMLDivElement>) => {
-    if (e.currentTarget.classList.contains('cat--reverse')) {
-      console.log('ending...');
-      setFacingDirection('right');
+    e.preventDefault();
+    if (
+      e.currentTarget.classList.contains("cat--reverse") &&
+      e.propertyName === "left"
+    ) {
+      setFacingDirection("right");
     }
   };
 
   return (
     <div
       id="nyanCatOverly"
-      className={`overlay cat-${positions[currentIndex]} ${facingDirection == 'right' ? '' : 'cat--reverse'}`}
+      className={`overlay cat-${positions[currentIndex]} ${facingDirection == "right" ? "" : "cat--reverse"}`}
       onTransitionEnd={onTransitionEnd}
-      // onTransitionStart={(e) => {
-      //   console.log('onTransitionStart');
-      //   console.dir(e.target);
-      // }}
-      // onTransitionEnd={(e) => {
-      //   console.log('onTransitionEnd');
-      //   console.dir(e.target);
-      // }}
     >
       <div
-        className={`rainbow ${facingDirection == 'right' ? '' : 'no-rainbow'}`}>
+        className={`rainbow ${facingDirection == "right" ? "" : "no-rainbow"}`}
+      >
         <div className="sprite" />
       </div>
       <button
-        aria-label={'nyan cat'}
-        className={'button--no-display'}
-        onClick={onClick}>
+        aria-label={"nyan cat"}
+        className={"button--no-display"}
+        onClick={onClick}
+      >
         <div className="cat">
           <div className="tail">
             <div className="sprite" />
