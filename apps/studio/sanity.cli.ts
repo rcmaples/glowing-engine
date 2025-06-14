eb9a2e7157835af99013bac935519f412841654c
RC Maples <rc@rcmaples.io>
2025-06-14 11:49:31 -0400

    opinionated + previous files

--- apps/studio/sanity.cli.ts
+++ apps/studio/sanity.cli.ts
@@ -3,11 +3,7 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'uhpaa149',
-    dataset: 'glowing-engine'
+    dataset: 'glowing-engine',
   },
-  /**
-   * Enable auto-updates for studios.
-   * Learn more at https://www.sanity.io/docs/cli#auto-updates
-   */
   autoUpdates: true,
 })
