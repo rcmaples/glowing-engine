b8ec08cada56a46b41db51f2fb3bf10e9451895d
RC Maples <rc@rcmaples.io>
2025-06-19 12:34:30 -0400

    cleaning up vars

--- packages/sanity-shared/token.ts
+++ packages/sanity-shared/token.ts
@@ -1,8 +1,7 @@
 import 'server-only'
 
-// export const token = process.env.SANITY_API_READ_TOKEN
-export const token = process.env.NEXT_PUBLIC_SANITY_READ_TOKEN
+export const token = process.env.SANITY_API_READ_TOKEN
 
 if (!token) {
-  throw new Error('Missing NEXT_PUBLIC_SANITY_READ_TOKEN')
+  throw new Error('Missing SANITY_API_READ_TOKEN')
 }
