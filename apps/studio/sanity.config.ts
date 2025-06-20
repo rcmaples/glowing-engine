e8c1b4fd470bd147d85087638f482d7efa9fe404
RC Maples <rc@rcmaples.io>
2025-06-19 20:02:34 -0400

    functioning assistant

--- apps/studio/sanity.config.ts
+++ apps/studio/sanity.config.ts
@@ -3,6 +3,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 
 import {schemaTypes} from './src/schemaTypes'
+import {groqAssistant} from './src/plugins/groq-assistant'
 
 export default defineConfig({
   name: 'glowing-engine',
@@ -11,7 +12,7 @@ export default defineConfig({
   projectId: 'uhpaa149',
   dataset: 'glowing-engine',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), groqAssistant()],
 
   schema: {
     types: schemaTypes,
