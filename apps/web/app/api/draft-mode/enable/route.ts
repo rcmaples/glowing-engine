82c538c4b94ede4250c6dc8687ee1221d363077c
RC Maples <rc@rcmaples.io>
2025-07-17 08:45:14 -0400

    updating import statement [skip deploy]

--- apps/web/app/api/draft-mode/enable/route.ts
+++ apps/web/app/api/draft-mode/enable/route.ts
@@ -1,6 +1,6 @@
 import {defineEnableDraftMode} from 'next-sanity/draft-mode'
 
-import {clientWithToken} from '../../../../lib/sanity/client'
+import {clientWithToken} from '@/lib/sanity/client'
 
 export const {GET} = defineEnableDraftMode({
   client: clientWithToken,
